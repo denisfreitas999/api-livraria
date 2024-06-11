adc2bfe4bf1af891bcbf37ed550708316b4dd675
Denisson Freitas <freitasdenis_dev@hotmail.com>
2024-06-11 10:50:45 -0300

    Validação de campos de string para valores específicos no model e retorno de erros personalizados

--- api-livraria-parte_02/src/models/Livro.js
+++ api-livraria-parte_02/src/models/Livro.js
@@ -16,12 +16,16 @@ const livroSchema = new mongoose.Schema(
     },
     editora: {
       type: String,
-      required: [true, "A editora é obrigatória."]
+      required: [true, "A editora é obrigatória."],
+      enum: {
+        values: ["Casa do código", "Alura"],
+        message: "A editora {VALUE} não é um valor permitido."
+      }
     },
     numeroPaginas: {
       type: Number,
-      min: [10, "O número de páginas deve estar entre 10 e 5000."],
-      max: [5000, "O número de páginas deve estar entre 10 e 5000."]
+      min: [10, "O número de páginas deve estar entre 10 e 5000. Valor fornecido: {VALUE}"],
+      max: [5000, "O número de páginas deve estar entre 10 e 5000. Valor fornecido: {VALUE}"]
     }
   });
 
