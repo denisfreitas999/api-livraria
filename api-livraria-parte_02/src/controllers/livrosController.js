cb25360d5c0f261781e8c0a713eda30dad625e87
Denisson Freitas <freitasdenis_dev@hotmail.com>
2024-06-11 14:32:59 -0300

    Realizando paginação dos livros

--- api-livraria-parte_02/src/controllers/livrosController.js
+++ api-livraria-parte_02/src/controllers/livrosController.js
@@ -1,15 +1,26 @@
 import { AutorModel, LivroModel } from "../models/index.js";
 import NaoEncontrado from "../errors/NaoEncontrado.js"
+import RequisicaoIncorreta from "../errors/RequisicaoIncorreta.js";
 
 class LivroController {
 
   static listarLivros = async (req, res, next) => {
     try {
-      const livrosResultado = await LivroModel.find()
-        .populate("autor")
-        .exec();
+      let { limite = 5, pagina = 1 } = req.query;
+      limite = parseInt(limite);
+      pagina = parseInt(pagina);
 
-      res.status(200).json(livrosResultado);
+      if (limite > 0 && pagina > 0) {
+        const livrosResultado = await LivroModel.find()
+          .skip((pagina - 1) * limite)
+          .limit(limite)
+          .populate("autor")
+          .exec();
+
+        res.status(200).json(livrosResultado);
+      } else {
+        next(new RequisicaoIncorreta());
+      }
     } catch (erro) {
       next(erro);
     }
